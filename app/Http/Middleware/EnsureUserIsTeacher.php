c5b29f08ae9f16732baa8ceed11081f5e17d329e
Krystian Dziewa <krysdz107@gmail.com>
2020-09-22 23:40:38 +0200

    add new conditional to EnsureUserIsTeacher

--- app/Http/Middleware/EnsureUserIsTeacher.php
+++ app/Http/Middleware/EnsureUserIsTeacher.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\Group;
+use App\Models\Lesson;
 use App\Models\Teacher;
+use App\Models\User;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redirect;
@@ -18,10 +21,31 @@ class EnsureUserIsTeacher
      */
     public function handle(Request $request, Closure $next)
     {
+        /** @var User $user */
         $user = $request->user();
 
-        if (!$user || !Teacher::where(['user_id' => $user->id])->exists()) {
+        if (!$user) {
             return Redirect::route('login');
+        }
+
+        if (!$user->teacher()->exists()) {
+            return Redirect::route('login');
+        }
+
+        if (($groupId = $request->route()->parameter('groupId')) != null) {
+            $group = Group::find($groupId);
+
+            if (!empty($group) && $group->teacher_id !== $user->teacher->id) {
+                return Redirect::route('login');
+            }
+        }
+
+        if (($lessonId = $request->route()->parameter('lessonId')) != null) {
+            $lesson = Lesson::find($lessonId);
+
+            if (!empty($lesson) && $lesson->group->teacher_id !== $user->teacher->id) {
+                return Redirect::route('login');
+            }
         }
 
         return $next($request);
