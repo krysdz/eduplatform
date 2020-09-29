c0382b70d38176a7b78b4e8b9c650e961bda7ab6
Krystian Dziewa <krysdz107@gmail.com>
2020-09-30 00:35:14 +0200

    fix bug in admin navbar

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -6,29 +6,22 @@
             <a href="{{route('admin.users.index')}}" class="navbar-item">
                 Użytkownicy
             </a>
-        </div>
-        <div class="navbar-start">
             <a href="{{route('admin.terms.index')}}" class="navbar-item">
                 Semestry
             </a>
-        </div>
-        <div class="navbar-start">
             <a href="{{route('admin.faculties.index')}}" class="navbar-item">
                 Wydziały
             </a>
-        </div>
-        <div class="navbar-start">
             <a href="{{route('admin.courses.index')}}" class="navbar-item">
                 Kursy
             </a>
-        </div>
-        <div class="navbar-start">
             <a href="{{route('admin.groups.index')}}" class="navbar-item">
                 Grupy
             </a>
         </div>
     </div>
 @endsection
+
 @section('content')
 <h1>Sekcja administratora</h1>
 @endsection
